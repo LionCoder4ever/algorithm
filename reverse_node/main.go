58e69a012b06f8a42a7f9a574ccf5d819cdb02f7
kzwcoder <kzwcoder2015@163.com>
2020-01-22 22:26:16 +0800

    recursive and insert

--- reverse_node/main.go
+++ reverse_node/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type LNode struct {
 	Data int
@@ -40,10 +42,54 @@ func Reverse(node *LNode) {
 	node.Next = pre
 }
 
+func RecursiveReverseChild(node *LNode) *LNode {
+	if node == nil || node.Next == nil {
+		return node
+	}
+	newHeader := RecursiveReverseChild(node.Next)
+	node.Next.Next = node
+	node.Next = nil
+	return newHeader
+}
+
+func RecursiveReverse(node *LNode) {
+	header := node.Next
+	node.Next = RecursiveReverseChild(header)
+}
+
+func InsertReverse(node *LNode) {
+	if node == nil || node.Next == nil {
+		return
+	}
+	var cur, next *LNode
+	cur = node.Next.Next
+	// move the first node to last
+	node.Next.Next = nil
+	for cur != nil {
+		// save the next node
+		next = cur.Next
+		// put the first node after cur
+		cur.Next = node.Next
+		// put the cur after the header
+		node.Next = cur
+		cur = next
+	}
+}
+
 func main() {
 	head := &LNode{}
 	CreateLNodes(head, 5)
 	PrintLNodes(head)
 	Reverse(head)
+	PrintLNodes(head)
+
+	CreateLNodes(head, 7)
+	PrintLNodes(head)
+	RecursiveReverse(head)
+	PrintLNodes(head)
+
+	CreateLNodes(head, 9)
+	PrintLNodes(head)
+	InsertReverse(head)
 	PrintLNodes(head)
 }
