f225790d047f505b3834b0c0562595c1e98961dc
kzwcoder <kzwcoder2015@163.com>
2020-02-13 00:21:24 +0800

    resort nodes

---
+++ resort_node/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CreateLNodes(node *LNode, size int) {
+	cur := node
+	for i := 1; i < size; i++ {
+		cur.Next = &LNode{}
+		cur.Next.Data = i
+		cur = cur.Next
+	}
+}
+func PrintLNodes(node *LNode) {
+	for cur := node.Next; cur != nil; cur = cur.Next {
+		fmt.Printf("%d  ", cur.Data)
+	}
+	fmt.Println()
+}
+
+type LNode struct {
+	Data int
+	Next *LNode
+}
+
+func Reverse(node *LNode) *LNode {
+	if node == nil || node.Next == nil {
+		return node
+	}
+	var tmp, pre *LNode
+	for node != nil {
+		tmp = node.Next
+		node.Next = pre
+		pre = node
+		node = tmp
+	}
+	return pre
+}
+
+func FindMiddeleNode(node *LNode) *LNode {
+	if node == nil || node.Next == nil {
+		return node
+	}
+	fast, slow := node, node
+	var slowPre *LNode
+	for fast != nil && fast.Next != nil {
+		slowPre = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	// split the nodes
+	slowPre.Next = nil
+	return slow
+}
+
+func Resort(node *LNode) {
+	if node == nil || node.Next == nil {
+		return
+	}
+	cur := node.Next
+	//// reverse node
+	//// get size of list
+	//len := 0
+	//for cur.Next != nil {
+	//	len++
+	//	cur = cur.Next
+	//}
+	//cur = node
+	//for i:=0; i < len / 2;i++ {
+	//	cur = cur.Next
+	//}
+	//// the second list header
+	//l2 := cur.Next
+	//cur.Next = nil
+	//// reverse l2
+	//cur = node.Next
+	l2 := FindMiddeleNode(node.Next)
+	rl2 := Reverse(l2)
+	var tmp *LNode
+	for cur.Next != nil {
+		tmp = cur.Next
+		cur.Next = rl2
+		cur = tmp
+		tmp = rl2.Next
+		rl2.Next = cur
+		rl2 = tmp
+	}
+	cur.Next = rl2
+}
+
+func main() {
+	header := &LNode{}
+	CreateLNodes(header, 8)
+	PrintLNodes(header)
+	Resort(header)
+	PrintLNodes(header)
+}
