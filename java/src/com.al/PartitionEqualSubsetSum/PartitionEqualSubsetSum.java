b6027ef868af2ec62e155466e3c72b30aeb0e0d8
kzwcoder <kzwcoder2015@163.com>
2020-06-16 23:29:44 +0800

    lc #416 partition equal subset sum  #dp

--- java/src/com.al/PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
+++ java/src/com.al/PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
@@ -6,25 +6,25 @@ public class PartitionEqualSubsetSum {
             int n = nums.length;
             int sum = 0;
             for (int i = 0; i < n; i++) {
-                sum+=nums[i];
+                sum += nums[i];
             }
-            if(sum % 2 != 0) return false;
+            if (sum % 2 != 0) return false;
             sum = sum >> 1;
             // two state
-            boolean[][] dp = new boolean[n+1][sum+1];
+            boolean[][] dp = new boolean[n + 1][sum + 1];
             for (int i = 0; i <= n; i++) {
                 dp[i][0] = true;
             }
             for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= sum ; j++) {
+                for (int j = 1; j <= sum; j++) {
                     // negative index
-                    if (j-nums[i-1] < 0){
-                        dp[i][j] = dp[i-1][j];
+                    if (j - nums[i - 1] < 0) {
+                        dp[i][j] = dp[i - 1][j];
                     } else {
                         // selections
                         // dont put sums[i] in the count
                         // if put sums[i], the pre count must be  "j-nums[i-1]"
-                        dp[i][j] = dp[i-1][j] | dp[i-1][j-nums[i-1]];
+                        dp[i][j] = dp[i - 1][j] | dp[i - 1][j - nums[i - 1]];
                     }
                 }
             }
@@ -34,9 +34,9 @@ public class PartitionEqualSubsetSum {
 
     public static void main(String[] args) {
         Solution sl = new PartitionEqualSubsetSum().new Solution();
-        System.out.println(sl.canPartition(new int[]{1,5,11,5}));
-        System.out.println(sl.canPartition(new int[]{1,2,3,5}));
-        System.out.println(sl.canPartition(new int[]{1,5,11,5,6,6}));
-        System.out.println(sl.canPartition(new int[]{1,2,5}));
+        System.out.println(sl.canPartition(new int[]{1, 5, 11, 5}));
+        System.out.println(sl.canPartition(new int[]{1, 2, 3, 5}));
+        System.out.println(sl.canPartition(new int[]{1, 5, 11, 5, 6, 6}));
+        System.out.println(sl.canPartition(new int[]{1, 2, 5}));
     }
 }
