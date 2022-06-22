d58bde35b6f95ea57249bfb93a07b5c95f38eb61
LionCoder4ever <kzwcoder2015@163.com>
2022-06-22 23:46:59 +0800

    commit #169

---
+++ cpp/169.多数元素.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=cpp
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1;
+        int count = 0;
+        for (int i=0;i<nums.size();i++) {
+            if (count == 0) {
+                candidate = nums[i];
+            }
+            if (candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
+// @lc code=end
+
