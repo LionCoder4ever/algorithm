d8ec54df9e2a7fa3ce304db23bdf9853920295dc
LionCoder4ever <kzwcoder2015@163.com>
2022-07-02 20:10:19 +0800

    commit #122 状态压缩

--- cpp/122.买卖股票的最佳时机-ii.cpp
+++ cpp/122.买卖股票的最佳时机-ii.cpp
@@ -9,19 +9,23 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2, 0));
-        int ans = 0;
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
+
+        // 不持有
+        int p0 = 0;
+        // 持有
+        int p1 = -prices[0];
+
         for (int i =1;i<n;i++) {
             // 不持有
-            dp[i][0] = max(dp[i-1][0], prices[i] + dp[i-1][1]);
+            int c0 = max(p0, prices[i] + p1);
             // 持有
-            dp[i][1] = max(dp[i-1][0] -prices[i], dp[i-1][1] );
-            ans = max(ans, dp[i][0]);
+            int c1 = max(p1, p0 - prices[i]);
+            p0 = c0;
+            p1 = c1;
         }
-        return ans;
+        return p0;
     }
+    
 };
 // @lc code=end
 
