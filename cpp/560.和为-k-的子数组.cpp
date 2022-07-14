baadb89ee588cdc3ee6fd899419faf1bc874af81
LionCoder4ever <kzwcoder2015@163.com>
2022-07-15 00:24:16 +0800

    commit #560

--- cpp/560.和为-k-的子数组.cpp
+++ cpp/560.和为-k-的子数组.cpp
@@ -10,20 +10,20 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> s(n+1);
-        unordered_map<int,int> map;
+        unordered_map<int,int> map{{0,1}};
         map[0] = 1;
         // 前缀和
         int ans = 0;
         for (int i=1;i<=n;i++) {
             s[i] = s[i-1] + nums[i-1];
-            if (map.find(s[i]-k)!= map.end()) {
+            if (map.find(s[i]-k) != map.end()) {
                 ans += map[s[i]-k];
             }
             map[s[i]]++;
         }
-        // for (auto i:s) {
-        //     cout << i << endl;
-        // }
+        for (auto i:s) {
+            cout << i << endl;
+        }
         return ans;
     }
 };
